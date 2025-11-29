d09cecaffe396d4c876c352b4176d04e041dccb3
Md Rahul Ahmed <rahul191ar@gmail.com>
2025-11-29 15:38:40 +0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-2025"
+    key            = "prod/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket"state_bucket" {
+  bucket = "terraform-state-2025"
+  force_destroy = true
+}
+
+resource "aws_dynamodb_table" "lock_table" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-0dee22c13ea7a9a60"  # Amazon Linux 2 Mumbai
+  instance_type = "t3.micro"
+  tags = {
+    Name = "fix-broke-tf"
+  }
+}
